cbdfb2aee3edc0070af4b4e7ff25540d4adbbb93
Lee Presswood <leepresswood@carfax.com>
2017-04-20 11:44:38 -0500

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Router, Route, IndexRoute} from 'react-router';
+
 import './App.css';
 import HelloWorldList from './helloworldlist/HelloWorldList';
 
 const App = () => {
     return (
-        <div className="App">
-            <HelloWorldList />
-        </div>
+        <Router>
+            <Route path="/" component={Main} path="app">
+                <IndexRoute component={Home}/>
+                <Route path="/cars" component={Car}/>
+                <Route path="/about" component={About}/>
+            </Route>
+        </Router>
     );
 };
 
