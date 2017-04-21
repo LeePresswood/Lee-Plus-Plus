d9b8b41d0193b73794ea7ecf4e281038085b918e
Lee Presswood <leepresswood@carfax.com>
2017-04-21 09:18:36 -0500

    LinkList works. Yay.

--- src/pages/Topics.js
+++ src/pages/Topics.js
@@ -5,11 +5,14 @@ import LinkList from "./_components/linklist/LinkList";
 
 class Topics extends Component {
     render() {
+        let popularTagsData = [1,2,3,4,5];
+        let allTagsData = [1,2,3,4,5,6,7,8,9,10,11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+
         return (
             <div id="Topics">
                 <Navbar/>
-                <LinkList headerText="Popular Topics"/>
-                <LinkList headerText="All Topics"/>
+                <LinkList headerText="Popular Topics" columns={2} tags={popularTagsData}/>
+                <LinkList headerText="All Topics" columns={3} tags={allTagsData}/>
             </div>
         );
     }
--- src/pages/_components/linklist/LinkList.js
+++ src/pages/_components/linklist/LinkList.js
@@ -2,12 +2,42 @@ import React, {Component} from 'react';
 import './LinkList.css';
 
 class LinkList extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.addGreeting = this.addGreeting.bind(this);
-    //
-    //     this.state = {greetings: ['Jim', 'Sally', 'Bender']};
-    // }
+    constructor(props) {
+        super(props);
+        this.getBootstrapColumn = this.getBootstrapColumn.bind(this);
+        this.buildDataRows = this.buildDataRows.bind(this);
+
+        // this.state = {greetings: ['Jim', 'Sally', 'Bender']};
+    }
+
+    getBootstrapColumn() {
+        switch (this.props.columns) {
+            case 1:
+                return "col-sm-12";
+            case 2:
+                return "col-sm-6";
+            case 3:
+                return "col-sm-4";
+            case 4:
+                return "col-sm-3";
+            case 6:
+                return "col-sm-2";
+            case 12:
+                return "col-sm-1";
+            default:
+                return "hidden";
+        }
+    }
+
+    buildDataRows() {
+        console.log(this.props);
+        let dataTags = this.props.tags.map((dataTag, index) =>
+            <div className={this.getBootstrapColumn()} key={index}>{dataTag}</div>
+        );
+
+        return <row>{ dataTags }</row>
+    }
+
     //
     // renderGreetings() {
     //     return this.state.greetings.map(name => (
@@ -22,16 +52,17 @@ class LinkList extends Component {
     render() {
         return (
             <div className="LinkList">
-                <div className="container">
+                <div className="container margin-20 margin-20">
                     <div className="row">
                         <div className="col-sm-12">
                             <label>{this.props.headerText}</label>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-sm-4">a</div>
-                        <div className="col-sm-4">b</div>
-                        <div className="col-sm-4">c</div>
+                        {this.buildDataRows()}
+                        {/*<div className="col-sm-4">a</div>*/}
+                        {/*<div className="col-sm-4">b</div>*/}
+                        {/*<div className="col-sm-4">c</div>*/}
                     </div>
                 </div>
             </div>
