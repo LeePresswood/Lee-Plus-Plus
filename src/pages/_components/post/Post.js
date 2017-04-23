40f7e395f283426238c2d2f087dc1387e900bf83
Lee Presswood <LeePresswood@gmail.com>
2017-04-23 12:10:51 -0500

    Found a way to write code with syntax highlighting.

--- src/pages/_components/post/Post.js
+++ src/pages/_components/post/Post.js
@@ -4,12 +4,16 @@ import React, {Component} from 'react';
 import './Post.css';
 
 class Post extends Component {
+    writePostTextAsHtml(){
+        return {__html: this.props.post.text}
+    }
+    
     render() {
         return (
             <div className="col-sm-12 margin-10 post" key={this.props.post.title}>
                 <p className="postTitle">{this.props.post.title}</p>
                 <p className="postWrittenOn">{this.props.post.writtenOn}</p>
-                <p className="postText">{this.props.post.text}</p>
+                <p className="postText" dangerouslySetInnerHTML={this.writePostTextAsHtml()}></p>
             </div>
         );
     }
