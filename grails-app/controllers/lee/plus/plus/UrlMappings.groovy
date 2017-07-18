917b02ae50684d770bada23fc65d0ad183376334
Lee Presswood <LeePresswood@gmail.com>
2017-07-18 00:05:40 -0500

    Went to a RESTful post view.

--- grails-app/controllers/lee/plus/plus/UrlMappings.groovy
+++ grails-app/controllers/lee/plus/plus/UrlMappings.groovy
@@ -13,6 +13,7 @@ class UrlMappings {
         "/"(uri: "/pages/1")
         "/pages/$currentPage"(controller: "blogPost", action: "getPage")
         
+        "/pages/$currentPage/posts/$postId"(controller: "blogPost", action: "getPost")
         
         "500"(view:'/error')
         "404"(view:'/notFound')
