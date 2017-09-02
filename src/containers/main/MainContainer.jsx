e65877daced3d5360e5af8f4eacad9b155be4395
Lee Presswood <LeePresswood@gmail.com>
2017-09-02 16:20:20 -0500

    Added flexbox.

--- src/containers/main/MainContainer.jsx
+++ src/containers/main/MainContainer.jsx
@@ -5,14 +5,14 @@ class MainContainer extends Component {
     render() {
         return (
             <div className="MainContainer">
-                <div className="about-me-column box-with-shadow">
-    
-                </div>
                 <div className="blog-column box-with-shadow">
                     <div className="title-box">
                         <h1 className="title">Java Development Tutorial: Part 3</h1>
                         <h2 className="subtitle">The Garbage Collector</h2>
                     </div>
+                </div>
+                <div className="about-me-column box-with-shadow">
+    
                 </div>
             </div>
         );
