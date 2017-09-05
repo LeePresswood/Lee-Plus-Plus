e3f2e374fbd68a9770e8795fed2f656e2ebdaf1f
Lee Presswood <LeePresswood@gmail.com>
2017-09-05 00:49:44 -0500

    Fighting with testing.

--- src/test/MainContainer.test.js
+++ src/test/MainContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainContainer from './MainContainer';
+import MainContainer from '../containers/main/MainContainer';
 
 it('renders without crashing', () =>{
     const div = document.createElement('div');
