b83a90db05bda8f5785a44a5026553e98686bdf5
Lee Presswood <LeePresswood@gmail.com>
2017-09-04 23:47:26 -0500

    Moved App component to use class notation.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from "../components/navbar/Navbar";
 import Content from "./content/Content";
 import Footer from "../components/footer/Footer";
 
-const App = () => (
-    <div className="App">
-        <Navbar/>
-        <Content/>
-        <Footer/>
-    </div>
-);
+class App extends Component {
+    render(){
+        return (
+            <div className="App">
+                <Navbar/>
+                <Content/>
+                <Footer/>
+            </div>
+        );
+    }
+}
 
 export default App;
