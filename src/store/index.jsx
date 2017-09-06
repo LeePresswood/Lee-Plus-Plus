91ddbb854c6ff1bbb391f591266d68c5cd5eb59f
Lee Presswood <LeePresswood@gmail.com>
2017-09-05 23:14:03 -0500

    Added Axios and Axios middleware.

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,20 +1,28 @@
 import {applyMiddleware, compose, createStore} from 'redux'
 import {routerMiddleware} from 'react-router-redux'
 import thunk from 'redux-thunk'
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from '../reducers/index'
 
-export const history = createHistory()
+export const history = createHistory();
 
-const initialState = {}
-const enhancers = []
+const client = axios.create({ //all axios can be used, shown in axios documentation
+    baseURL : 'http://localhost:3001/',
+    responseType : 'json'
+});
+
+const initialState = {};
+const enhancers = [];
 const middleware = [
     thunk,
+    axiosMiddleware(client),
     routerMiddleware(history)
-]
+];
 
 if(process.env.NODE_ENV === 'development'){
-    const devToolsExtension = window.devToolsExtension
+    const devToolsExtension = window.devToolsExtension;
     
     if(typeof devToolsExtension === 'function'){
         enhancers.push(devToolsExtension())
@@ -24,12 +32,12 @@ if(process.env.NODE_ENV === 'development'){
 const composedEnhancers = compose(
     applyMiddleware(...middleware),
     ...enhancers
-)
+);
 
 const store = createStore(
     rootReducer,
     initialState,
     composedEnhancers
-)
+);
 
 export default store
