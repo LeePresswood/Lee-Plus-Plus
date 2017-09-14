a82a31a13d09be579de95c2e0373ab1094736c65
Lee Presswood <LeePresswood@gmail.com>
2017-09-14 17:48:24 -0500

    Moved to a better code highlighting style

--- src/components/PostContentBox.jsx
+++ src/components/PostContentBox.jsx
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import {agate} from 'react-syntax-highlighter/dist/styles';
+import {androidstudio} from 'react-syntax-highlighter/dist/styles';
 import '../styles/PostContentBox.css';
 
 class PostContentBox extends Component {
     mapParagraphsToTags(){
         return this.props.body.map((segment, index) => {
             if(segment.isCode)
-                return <SyntaxHighlighter key={index} language={segment.language} style={agate}>{segment.text}</SyntaxHighlighter>;
+                return <SyntaxHighlighter
+                    key={index}
+                    language={segment.language}
+                    style={androidstudio}>{segment.text}</SyntaxHighlighter>;
             else if(segment.isHeader)
-                return <h1 key={index}>{segment.text}</h1>;
-            return <p key={index}>{segment.text}</p>;
+                return <h1
+                    key={index}>{segment.text}</h1>;
+            return <p
+                key={index}>{segment.text}</p>;
         })
     }
     
