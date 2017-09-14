fc6019b3d070a7f8b669ee15bf6c454472ae7028
Lee Presswood <LeePresswood@gmail.com>
2017-09-14 14:20:12 -0500

    Cleaned up the post request reducer.

--- src/reducers/PostRequestReducer.jsx
+++ src/reducers/PostRequestReducer.jsx
@@ -12,62 +12,47 @@ export default (state = initialState, action) =>{
         case "FetchSinglePost":
             return {
                 ...state,
-                loadedPosts: []
-                // title : 'Loading',
-                // subtitle : 'Loading',
-                // dateTime : 'Loading',
-                // body : [{text: 'Loading'}],
-                // loading: true
+                title : 'Loading',
+                subtitle : 'Loading',
+                dateTime : 'Loading',
+                body : [{text: 'Loading'}],
+                loading: true
             };
         case "FetchSinglePost_SUCCESS":
             return {
                 ...state,
-                loadedPosts: action.payload.data
-                // title : action.payload.data.title,
-                // subtitle : action.payload.data.subtitle,
-                // dateTime : action.payload.data.dateTime,
-                // body : action.payload.data.body,
-                // loading: false
+                title : action.payload.data.title,
+                subtitle : action.payload.data.subtitle,
+                dateTime : action.payload.data.dateTime,
+                body : action.payload.data.body,
+                loading: false
             };
         case "FetchSinglePost_FAILURE":
             return {
                 ...state,
-                loadedPosts: []
-                // title : "Failed",
-                // subtitle : "Failed",
-                // dateTime : "Failed",
-                // body : [{text: "Failed"}],
-                // loading: false
+                title : "Failed",
+                subtitle : "Failed",
+                dateTime : "Failed",
+                body : [{text: "Failed"}],
+                loading: false
             };
         case "FetchMultiPost":
             return {
                 ...state,
-                loadedPosts: []
-                // title : 'Loading',
-                // subtitle : 'Loading',
-                // dateTime : 'Loading',
-                // body : [{text: 'Loading'}],
-                // loading: true
+                loadedPosts: [],
+                loading: true
             };
         case "FetchMultiPost_SUCCESS":
             return {
                 ...state,
-                loadedPosts: action.payload.data
-                // title : action.payload.data.title,
-                // subtitle : action.payload.data.subtitle,
-                // dateTime : action.payload.data.dateTime,
-                // body : action.payload.data.body,
-                // loading: false
+                loadedPosts: action.payload.data,
+                loading: false
             };
         case "FetchMultiPost_FAILURE":
             return {
                 ...state,
-                loadedPosts: []
-                // title : "Failed",
-                // subtitle : "Failed",
-                // dateTime : "Failed",
-                // body : [{text: "Failed"}],
-                // loading: false
+                loadedPosts: [],
+                loading: false
             };
         default:
             return state;
