5a184f520aecc5681f53ae450572a02b77fdb44b
Lee Presswood <LeePresswood@gmail.com>
2017-09-20 09:58:04 -0500

    Added floating navbar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import '../styles/Header.css';
+import logo from '../img/lpp-t-128.png';
+import logo_smaller from '../img/lpp-t-64.png';
 
 class Header extends Component {
+    
     render(){
         return (
             <header className="Navbar">
                 <div className="app-container">
                     <div className="left">
-                        <Link to="/" className="Name">Lee++</Link>
+                        <Link to="/" className="Name">
+                            <img src={logo} alt="Lee++ Logo" />
+                        </Link>
                     </div>
                     <nav className="right">
                         <Link to="/">Blog</Link>
