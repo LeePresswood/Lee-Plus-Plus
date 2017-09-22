95e8213bd68b2b671c923ae6084d29dd5fef4f2a
Lee Presswood <LeePresswood@gmail.com>
2017-09-22 00:13:54 -0500

    Made the navbar a lot cleaner.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import '../styles/Header.css';
-import logo from '../img/lpp-t-64.png';
+import logo from '../img/lpp-128.png';
 
 class Header extends Component {
     
     render(){
         return (
             <header className="Navbar">
-                <div className="app-container">
-                    <div className="left">
-                        <Link to="/" className="nav-link">
-                            <img src={logo} alt="Lee++ Logo" />
-                        </Link>
+                <div className="top">
+                    <div className="app-container">
+                        <div className="left">
+                            <Link to="/" className="nav-link">
+                                <img src={logo} alt="Lee++ Logo" />
+                            </Link>
+                        </div>
+                        <nav className="right">
+                            <Link to="/" className="nav-link nav-right-link">Blog</Link>
+                            <a href="https://github.com/LeePresswood" className="nav-link nav-right-link">GitHub</a>
+                            <Link to="/about" className="nav-link nav-right-link">About</Link>
+                        </nav>
                     </div>
-                    <nav className="right">
-                        <Link to="/" className="nav-link nav-right-link">Blog</Link>
-                        <a href="https://github.com/LeePresswood" className="nav-link nav-right-link">GitHub</a>
-                        <Link to="/about" className="nav-link nav-right-link">About</Link>
-                    </nav>
                 </div>
+                <div className="bottom">
+                    <div className="app-container">
+                    
+                    </div>
+                </div>
+                
             </header>
         );
     }
