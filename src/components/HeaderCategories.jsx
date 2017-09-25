ebf11b10c7c49d7976d87c36efa9e1cb600e9f53
Lee Presswood <LeePresswood@gmail.com>
2017-09-25 12:46:09 -0500

    Created new class for the HeaderCategories.

---
+++ src/components/HeaderCategories.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import '../styles/Header.css';
+import { fetchCategoriesAction } from "../actions/CategoryActions";
+
+class HeaderCategories extends Component {
+    componentWillMount(){
+        this.props.loadCategories();
+    }
+    
+    render(){
+        return (
+            <div className="HeaderCategories bottom">
+                <div className="app-container">
+                    <div className="row">
+                        {this.mapCategoriesToLinks()}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+    mapCategoriesToLinks(){
+        return this.props.categories.map((category, index) =>{
+            return category === 'Loading' ?
+                <p key={index}>Loading</p> :
+                <Link key={index} to={"/categories/" + category + "/pages/0"}>{category}</Link>;
+        });
+    }
+}
+
+HeaderCategories.propTypes = {
+    categories : PropTypes.arrayOf(PropTypes.string)
+};
+
+const mapStateToProps = state => ({
+    categories : state.categoryReducer.categories
+});
+
+const mapDispatchToProps = dispatch =>{
+    return {
+        loadCategories : () => dispatch(fetchCategoriesAction()),
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(HeaderCategories)
