81b63915fb890d4bb204e795c3e0941eb7b752a3
Lee Presswood <LeePresswood@gmail.com>
2017-09-25 13:19:09 -0500

    Extracted navigation bar to its own component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import '../styles/Header.css';
-import logo from '../img/lpp-128.png';
+import HeaderNavigation from "./HeaderNavigation";
 import HeaderCategories from "./HeaderCategories";
 
 class Header extends Component {
     render(){
         return (
             <header className="Header">
-                <div className="top">
-                    <div className="app-container">
-                        <div className="left">
-                            <Link to="/">
-                                <img src={logo} alt="Lee++ Logo"/>
-                            </Link>
-                        </div>
-                        <nav className="right">
-                            <Link to="/">Blog</Link>
-                            <a href="https://github.com/LeePresswood">GitHub</a>
-                            <Link to="/about">About</Link>
-                        </nav>
-                    </div>
-                </div>
+                <HeaderNavigation/>
                 <HeaderCategories/>
             </header>
         );
---
+++ src/components/HeaderNavigation.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/Header.css';
+import logo from '../img/lpp-128.png';
+
+class HeaderNavigation extends Component{
+    render(){
+        return (
+            <div className="HeaderNavigation top">
+                <div className="app-container">
+                    <div className="left">
+                        <Link to="/">
+                            <img src={logo} alt="Lee++ Logo"/>
+                        </Link>
+                    </div>
+                    <nav className="right">
+                        <Link to="/">Blog</Link>
+                        <a href="https://github.com/LeePresswood">GitHub</a>
+                        <Link to="/about">About</Link>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HeaderNavigation;
