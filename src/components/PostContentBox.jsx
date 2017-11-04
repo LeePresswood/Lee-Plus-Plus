6dc71ab37680cc74fa1b5ca2bf26bf99754dfa75
Lee Presswood <LeePresswood@gmail.com>
2017-11-04 18:44:55 -0500

    Added margin staggering to the article.

--- src/components/PostContentBox.jsx
+++ src/components/PostContentBox.jsx
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { railscasts } from 'react-syntax-highlighter/dist/styles';
-import '../styles/PostContentBox.css';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { monoBlue } from "react-syntax-highlighter/dist/styles";
+import "../styles/PostContentBox.css";
 
 class PostContentBox extends Component {
-    mapParagraphsToTags(){
-        return this.props.body.map((segment, index) =>{
-            if(segment.isCode){
-                return <SyntaxHighlighter
-                    key={index}
-                    language={segment.language}
-                    style={railscasts}
-                    showLineNumbers
-                >{segment.text}</SyntaxHighlighter>;
-            }
-            else if(segment.isHeader){
-                return <h1
-                    key={index}>{segment.text}</h1>;
-            }
-            else{
-                return <p
-                    key={index}>{segment.text}</p>;
-            }
-        })
-    }
-    
-    render(){
-        return (
-            <div className="PostContentBox">
-                {this.mapParagraphsToTags()}
-            </div>
-        );
-    }
+  mapParagraphsToTags() {
+    return this.props.body.map((segment, index) => {
+      if(segment.isCode){
+        return <SyntaxHighlighter
+          key={ index }
+          language={ segment.language }
+          style={ monoBlue }
+          showLineNumbers
+        >{ segment.text }</SyntaxHighlighter>;
+      }
+      else if(segment.isHeader){
+        return <h1
+          key={ index }>{ segment.text }</h1>;
+      }
+      else{
+        return <p
+          key={ index }>{ segment.text }</p>;
+      }
+    });
+  }
+  
+  render() {
+    return (
+      <div className="PostContentBox">
+        { this.mapParagraphsToTags() }
+      </div>
+    );
+  }
 }
 
 PostContentBox.propTypes = {
-    body : PropTypes.arrayOf(PropTypes.object),
+  body : PropTypes.arrayOf(PropTypes.object),
 };
 
 export default PostContentBox;
